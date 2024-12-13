f005b7b14c0b8f329012f209237d2e2ea68be95d
Spark2a <90321704+Spark2a@users.noreply.github.com>
2024-12-13 15:59:08 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import List from "./pages/List";
 import Details from "./pages/Details";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/AnimeList"> {/*basename */}
+    <Router basename="/AnimeList">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
         <Route path="/details/:id" element={<Details />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirige vers la page d'accueil si la route ne correspond à rien */}
       </Routes>
     </Router>
   );
